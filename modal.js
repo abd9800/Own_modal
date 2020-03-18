89d52df71db7bc8cc11ad2d462d0ec36585fcf5e
abd9800 <57014274+abd9800@users.noreply.github.com>
2020-03-18 14:22:02 +0530

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,13 +1,16 @@
 const hidemodal = () => {
 document.querySelector('.modal')
-  .classList.toggle(modal_hide);
+  .classList.toggle('modal_hide');
 };
+
 document.querySelector('#show')
 .addEventListener('click',hidemodal);
+
 document.querySelector('#clicknow')
-.addEventListener('click',(event) => {
+.addEventListener('submit',(event) => {
   event.preventDefault();
   hidemodal();
 });
+
 document.querySelector('.modal_close span')
 .addEventListener('click',hidemodal);
